7ffcf53c6ff1671f263e144886171392cdaffa20
shelbourn <matthew@shelbourn.io>
2020-04-05 16:30:07 -0700

    Initial commit ::rocket::

---
+++ Source/degree.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>;
+
+// Initializing enumerated data type values for the Degree Programs
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
+// Initializing an array of strings that matches the enumerated data types for printing
+static const std::string degreeProgramArray[] = { "SECURITY", "NETWORK", "SOFTWARE" };
