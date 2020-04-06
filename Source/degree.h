f4635f112545ae0206c660b50f6bc76b26c4d20a
shelbourn <matthew@shelbourn.io>
2020-04-06 14:39:17 -0700

    Building out the parser

--- Source/degree.h
+++ Source/degree.h
@@ -5,4 +5,4 @@
 enum class DegreeProgram { SECURITY, NETWORK, SOFTWARE };
 
 // Initializing an array of strings that matches the enumerated data types for printing
-static const std::string degreeProgramArray[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+// static const std::string degreeProgramArray[] = { "SECURITY", "NETWORK", "SOFTWARE" };
--- Source/roster.cpp
+++ Source/roster.cpp
@@ -11,6 +11,7 @@ Roster::Roster()
 }
 
 // Defining the non-empty roster constructor -- sets the max size for the roster
+// Allocates appropriate memory for roster size
 Roster::Roster(int capacity)
 {
 	this->capacity = capacity;
@@ -18,8 +19,41 @@ Roster::Roster(int capacity)
 	this->classRosterArray = new Student * [capacity];
 }
 
+// Return student record at specified record number (index)
 Student* Roster::getStudentRecordAt(int index)
 {
 	return classRosterArray[index];
 }
 
+// Parse string data and add it to roster
+void Roster::parseString(string record)
+{
+	if (lastIndex < capacity) {
+		lastIndex++;
+
+		// Validates DegreeProgram in submitted string
+		DegreeProgram degree;
+		if (record[6] == 'SE') degree = DegreeProgram::SECURITY;
+		else if (record[6] == 'NE') degree = DegreeProgram::NETWORK;
+		else if (record[6] == 'SO') degree = DegreeProgram::SOFTWARE;
+		else
+		{
+			cerr << "INVALID DEGREE PROGRAM ENTERED! EXITING PROGRAM!\n";
+			exit(-1);
+		}
+
+		// Parse through each roster record (string)
+		// Extract each substring (separated by commas) and store them in temporary variables
+
+		// Extracting the Student ID
+		int rightSubStringEnd = record.find(",");
+		string studentID = record.substr(0, rightSubStringEnd);
+
+		// Extracting First Name
+		int leftSubStringEnd = rightSubStringEnd + 1;
+		rightSubStringEnd = record.find(",", leftSubStringEnd);
+		string firstName = record.substr(leftSubStringEnd, rightSubStringEnd - leftSubStringEnd);
+
+
+	}
+}
