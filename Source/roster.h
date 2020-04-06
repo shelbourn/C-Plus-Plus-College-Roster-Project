2d115bd84c021d9faeba587f51c9702a273bb162
shelbourn <matthew@shelbourn.io>
2020-04-06 13:28:28 -0700

    Building out roster.h

--- Source/roster.h
+++ Source/roster.h
@@ -8,17 +8,17 @@
 class Roster
 {
 public:
-	Roster();
-	~Roster();
+	Roster(); // Empty constructor to set the Roster to default values
+	Roster(int capacity); // Sets the max size (number of records) for the Roster
+
+	Student* getStudentRecordAt(int index);
+
+	void parseStringAdd(string studentRecord); // Parses string, creates Student object, adds record to roster
+	void addStudentRecord(string studentID, string firstName, string lastName, string emailAddress, int age, int daysToComplete1, int daysToComplete2, int daysToComplete3, DegreeProgram degree);
+	void printAllRecords(); // Prints all student records in the roster
+	bool removeStudentRecord(string studentID); // Removes a student record based on studentID
+
 
 private:
 
 };
-
-Roster::Roster()
-{
-}
-
-Roster::~Roster()
-{
-}
