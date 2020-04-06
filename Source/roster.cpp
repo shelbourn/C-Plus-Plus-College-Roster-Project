8e802c305d77ea3029d5c032b1944fa0379c945d
shelbourn <matthew@shelbourn.io>
2020-04-06 14:07:25 -0700

    Defining methods and constructors for roster.cpp

--- Source/roster.cpp
+++ Source/roster.cpp
@@ -9,3 +9,17 @@ Roster::Roster()
 	this->lastIndex = -1; // Arrays are 0-indexed so -1 will set it to no value
 	this->classRosterArray = nullptr;
 }
+
+// Defining the non-empty roster constructor -- sets the max size for the roster
+Roster::Roster(int capacity)
+{
+	this->capacity = capacity;
+	this->lastIndex = -1;
+	this->classRosterArray = new Student * [capacity];
+}
+
+Student* Roster::getStudentRecordAt(int index)
+{
+	return classRosterArray[index];
+}
+
